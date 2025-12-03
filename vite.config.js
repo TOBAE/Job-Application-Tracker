b91030abacbdd4e52ca06b50629e58e29881e34f
tobae <assumptaodibeli@gmail.com>
2025-12-03 17:23:06 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+const isGitHubPages = process.env.GITHUB_REPOSITORY?.includes("Job-Application-Tracker");
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Job-Application-Tracker/", 
+  base: isGitHubPages ? "/Job-Application-Tracker/" : "/",
 })
